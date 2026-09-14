ed85f9fcc302b4f00cafd0d0961c6e8afda10c08
Yohanam <rafaelcastellarifig@gmail.com>
2026-09-14 11:31:03 -0300

    Add files via upload

---
+++ Trabalho1 2026.2/src/cdg/inatel/loja/computador/HardwareBasico.java
@@ -0,0 +1,19 @@
+package cdg.inatel.loja.computador;
+
+public class HardwareBasico {
+    private String nome;
+    private float capacidade;
+
+    public HardwareBasico(String nome, float capacidade) {
+        this.nome = nome;
+        this.capacidade = capacidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getCapacidade() {
+        return capacidade;
+    }
+}
